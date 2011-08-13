d36dda552929530f50a652db95d3dd6a6bbe7e9c
= <matthewdhull@gmail.com>
2011-08-13 16:14:19 -0600

    added condition to check for session variables prior to generating csv file

--- downloadables/download.php
+++ downloadables/download.php
@@ -1,22 +1,26 @@
 <?php
 
-include "../Classes/ReportsClass.php";
+session_start();
+session_cache_limiter('nocache');
 
-$data = array();
-
-$data = Reports::cumulativeTestStats();
-
-$fp = fopen('testStatistics.csv', 'w');
-
-foreach ($data as $row) {
-    fputcsv($fp, $row);
+if(isset($_SESSION['name'])){
+	include "../Classes/ReportsClass.php";
+	
+	$data = array();
+	
+	$data = Reports::cumulativeTestStats();
+	
+	$fp = fopen('testStatistics.csv', 'w');
+	
+	foreach ($data as $row) {
+	    fputcsv($fp, $row);
+	}
+	
+	fclose($fp);
+	
+	header('Content-disposition: attachment; filename=testStatistics.csv');
+	header('Content-type: text/csv');
+	readfile("testStatistics.csv");
 }
-
-fclose($fp);
-
-header('Content-disposition: attachment; filename=testStatistics.csv');
-header('Content-type: text/csv');
-readfile("testStatistics.csv");
-
 
 ?>
