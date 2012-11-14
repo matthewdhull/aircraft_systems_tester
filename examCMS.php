8fd984172c0cde89525756ee517e8de7f0d65429
matthewdhull <matthewdhull@gmail.com>
2012-11-13 19:48:14 -0600

    added L3 to rqual options in examCMS.php

--- examCMS.php
+++ examCMS.php
@@ -653,6 +653,7 @@
 				$("#qualCode").change(function(){
 					var l5 = "<option value='l5'>L5</option>";
 					var l4 = "<option value='l4'>L4</option>";
+					var l3 = "<option value='l3'>L3</option>";
 					$("#syllabus option").remove();
 					if($(this).val() == "nh"){
 						$("#syllabus").append(l5);
@@ -661,7 +662,7 @@
 						$("#syllabus").append(l4);
 					}
 					if($(this).val() == "rqual"){
-						$("#syllabus").append(l5 + l4);
+						$("#syllabus").append(l5 + l4 + l3);
 					}
 					if($(this).val() == "trans"){
 						$("#syllabus").append(l4);
@@ -746,8 +747,8 @@
 								<option value="8">8</option>
 								<option value="9">9</option>
 								<option value="10">10</option>
-								<option value="11">11</option>																																		<option value="12">12</option>
-								<option value="13">13</option>																																		<option value="14">14</option>
+								<option value="11">11</option>																														<option value="12">12</option>
+								<option value="13">13</option>																														<option value="14">14</option>
 								<option value="15">15</option>
 								<option value="15">15</option>
 								<option value="16">16</option>
@@ -768,48 +769,6 @@
 								<option value="31">31</option>
 							</select>
 							<select id="classDateYear">
-								<option value="1970">1970</option>
-								<option value="1971">1971</option>
-								<option value="1972">1972</option>
-								<option value="1973">1973</option>
-								<option value="1974">1974</option>
-								<option value="1975">1975</option>
-								<option value="1976">1976</option>
-								<option value="1977">1977</option>
-								<option value="1978">1978</option>
-								<option value="1979">1979</option>
-								<option value="1980">1980</option>
-								<option value="1981">1981</option>
-								<option value="1982">1982</option>
-								<option value="1983">1983</option>
-								<option value="1984">1984</option>
-								<option value="1985">1985</option>
-								<option value="1986">1986</option>
-								<option value="1987">1987</option>
-								<option value="1988">1988</option>
-								<option value="1989">1989</option>
-								<option value="1990">1990</option>
-								<option value="1991">1991</option>
-								<option value="1992">1992</option>
-								<option value="1993">1993</option>
-								<option value="1994">1994</option>
-								<option value="1995">1995</option>
-								<option value="1996">1996</option>
-								<option value="1997">1997</option>
-								<option value="1998">1998</option>
-								<option value="1999">1999</option>
-								<option value="2000">2000</option>
-								<option value="2001">2001</option>
-								<option value="2002">2002</option>
-								<option value="2003">2003</option>
-								<option value="2004">2004</option>
-								<option value="2005">2005</option>
-								<option value="2006">2006</option>
-								<option value="2007">2007</option>
-								<option value="2008">2008</option>
-								<option value="2009">2009</option>
-								<option value="2010">2010</option>
-								<option value="2011">2011</option>
 								<option value="2012">2012</option>
 								<option value="2013">2013</option>
 								<option value="2014">2014</option>
