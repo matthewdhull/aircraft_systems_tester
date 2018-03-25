269e5c6c302ee76af9506ee28a0c2cb1ba621068
matthewdhull <matthewdhull@gmail.com>
2018-03-25 14:07:57 +0200

    added re-order to phase

--- taskModeling.php
+++ taskModeling.php
@@ -20,6 +20,51 @@
 
     $(document).ready(function(){
         
+        jQuery.fn.sortElements = (function(){
+     
+        var sort = [].sort;
+     
+        return function(comparator, getSortable) {
+     
+            getSortable = getSortable || function(){return this;};
+     
+            var placements = this.map(function(){
+     
+                var sortElement = getSortable.call(this),
+                    parentNode = sortElement.parentNode,
+     
+                    // Since the element itself will change position, we have
+                    // to have some way of storing its original position in
+                    // the DOM. The easiest way is to have a 'flag' node:
+                    nextSibling = parentNode.insertBefore(
+                        document.createTextNode(''),
+                        sortElement.nextSibling
+                    );
+     
+                return function() {
+     
+                    if (parentNode === this) {
+                        throw new Error(
+                            "You can't sort elements if any one is a descendant of another."
+                        );
+                    }
+     
+                    // Insert before flag:
+                    parentNode.insertBefore(this, nextSibling);
+                    // Remove flag:
+                    parentNode.removeChild(nextSibling);
+     
+                };
+     
+            });
+     
+            return sort.call(this, comparator).each(function(i){
+                placements[i].call(getSortable.call(this));
+            });
+     
+        };
+     
+    })();        
         
         function loadSubtasks(task_id) {
             $.post("PHPScripts/getSubtasks.php", {
@@ -64,13 +109,16 @@
 			$.post("PHPScripts/getPhases.php", function(data){
     			$("div .phase").remove();
                 $.each(data, function(key,value){
-                    var html = "<div id="+value.id+" class='phase'>";
+                    var html = "<div id="+value.id+" class='phase' data-number="+value.number+">";
                     html += "<input type='text' class='phase_order_input' name='phase_order' value="+value.number+"></input>";
                     html += "<input type='text' class='phase_input' name='phase' value='"+value.name+"'></input>";
                     html += "<button class='savePhaseButton'>Save Phase</button>";
                     html += "<button class='addTaskButton'>+ Task</button>";
                     html += "</div>";    
-                    $(".addPhaseButton").before(html);     
+                    $(".addPhaseButton").before(html);  
+                    $("div .phase").sortElements(function(a, b){
+                        return $(a).attr("data-number") > $(b).attr("data-number") ? 1 : -1;
+                    });   
                     bindAddTaskButtonEvent();
                     bindSavePhaseEvent();                                                   
                     loadTasks(value.id);                    
